c10011c4b3702c79aadb818bd386139ecce605d6
Javier Andres Moreno Alva <jamorenoalva@gmail.com>
2022-01-24 00:43:25 -0500

    Reto 02 - Semana 05

---
+++ Semana5/Reto02/Reto02.js
@@ -0,0 +1,39 @@
+let TI = ""; /*tipo de hamburguesa*/
+let N = 0; /*numero de hamburguesas*/
+let TP = ""; /*Tipo de pago*/
+let PA = 0; /*Precio de hamburguesa*/
+let CA = 0; /*Cargo por el uso de tarjeta*/
+let TO = 0; /*Total sin cargo*/
+let TOT = 0; /*Total con cargo*/
+
+function totalizar(TI, TP, N) {
+  CA = 0;
+  if (TP == "credito") {
+    TO = precioHamburguesa(TI) * N;
+    CA = TO * 0.05;
+  } else {
+    TO = precioHamburguesa(TI) * N;
+    CA = 0;
+  }
+  TOT = (TO + CA);
+  return TOT;
+}
+
+function precioHamburguesa(hamburguesa) {
+  PA = 0;
+  switch (hamburguesa) {
+    case "Sencilla":
+      return (PA = 20);
+      break;
+    case "Doble":
+      return (PA = 25);
+      break;
+    case "Triple":
+      return (PA = 28);
+      break;
+    default:
+      return (PA = 0);
+  }
+}
+
+console.log(totalizar("Sencilla", "credito", 1));
