02ddc87b214b53e0c4e8b03ade7f19b035a6eded
Javier Andres Moreno Alva <jamorenoalva@gmail.com>
2022-01-30 19:39:18 -0500

    Reto 01 - Semana 06

---
+++ Semana6/Reto01/script.js
@@ -0,0 +1,11 @@
+let datos=[];
+
+function agregar() {
+  let datoIngresado = document.getElementById("ingreso").value;
+  // console.log("1 - " + datoIngresado);
+  datos.push(datoIngresado);
+  console.log(datos);
+  // return datoIngresado;
+}
+
+
