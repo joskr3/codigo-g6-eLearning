e33c6eeb2dba0f32318fefac9fbed26a3806d1a3
Josue Retamozo Vargas <josretamozovar@gmail.com>
2022-02-04 20:33:01 -0500

    trabajando con estilos en Bulma

--- Semana7/Bootstrap/Repaso/script.js
+++ Semana7/Bootstrap/Repaso/script.js
@@ -33,17 +33,21 @@ let arrayUsuarios = [
     }
 ]
 
-let contenedorInfo = document.getElementsByClassName("contenedor-info-usuario")[0];
-let contenedorOperaciones = document.getElementsByClassName("contenedor-operaciones")[0];
+let contenedorInfoUsuario = document.getElementById("contenedor-info-usuario");
+let contenedorOperaciones = document.getElementById("contenedor-operaciones")
 let contenedorEjecutarOperaciones = document.getElementsByClassName("contenedor-ejecutar-operaciones")[0];
 let contenedorBienvenida = document.getElementById("contenedor-bienvenida");
 let placeholderDeLaAlerta = document.getElementById('liveAlertPlaceholder')
 let activadorDeAlerta = document.getElementById('boton-ingresar')
+let contenedorDatosUsuario = document.getElementById("datos-usuario");
 
 window.onload = function () {
     contenedorEjecutarOperaciones.style.display = "none";
-    contenedorOperaciones.style.display = "none";
-    console.log(contenedorEjecutarOperaciones)
+    //contenedorOperaciones.style.display = "none";
+    contenedorInfoUsuario.style.display = "none";
+    //contenedorDatosUsuario.style.display = "none";
+   // console.log(contenedorEjecutarOperaciones)
+   console.log(contenedorInfoUsuario)
 }
 
 
@@ -64,7 +68,7 @@ function obtenerUsuario() {
     }
 
     let returnUser = usuarioValido && passwordValido ? datosUsuario : null
-
+    console.log(returnUser)
     return returnUser
 
 }
@@ -73,30 +77,23 @@ function iniciarSesion() {
     const usuario = obtenerUsuario();
 
     if (usuario) {
-        alerta("Bienvenido", "success");
+        alerta("Bienvenido " + usuario.nombre, "success");
 
+        // contenedorInfoUsuario.classList.remove("d-none");
+        // contenedorOperaciones.classList.add("d-flex", "flex-column");
 
-        // if(alerta){
-        //     dejarDeMostrarAlerta()
-        // }
+        contenedorInfoUsuario.style.display = "flex";
+        contenedorInfoUsuario.style.justifyContent = "center"
 
-        //dejarDeMostrarAlerta();
-
-
-        // console.log(contenedorInfo.classList)
-        contenedorInfo.classList.remove("d-none");
-        contenedorOperaciones.classList.add("d-flex", "flex-column");
         document.querySelector(".contenedor-retiro").style.display = "none";
         document.querySelector(".contenedor-deposito").style.display = "none";
         document.querySelector(".contenedor-consulta").style.display = "none";
         document.querySelector("#nombre-usuario").innerHTML = usuario.nombre || "undefined";
         document.querySelector("#username-usuario").innerHTML = usuario.usuario;
         document.querySelector("#email-usuario").innerHTML = usuario.email;
+        document.getElementById("login").style.display = "none";
     } else {
-        alerta("Usuario o contraseña incorrectos/ alerta1", "danger");
-        // if(alerta){
-        //     dejarDeMostrarAlerta()
-        // }
+        alerta("Usuario o contraseña incorrectos", "danger");
     }
 }
 
@@ -113,10 +110,24 @@ function iniciarSesion() {
 // }
 
 function alerta(mensaje, tipo) {
+    // estamos creando una etiqueta div
     const etiqueta_envoltorio = document.createElement('div')
-    etiqueta_envoltorio.innerHTML = '<div class="alert alert-' + tipo + ' alert-dismissible" role="alert">' + mensaje + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+    // en esta linea agregamos una clase al div
+    etiqueta_envoltorio.classList.add('notification', tipo === "success" ? "is-success" : "is-danger")
 
-    placeholderDeLaAlerta.append(etiqueta_envoltorio)
+    // creamos un nodo de boton(elemento/etiqueta button)
+    etiqueta_envoltorio.innerHTML = `<button class="delete" id="boton-cerrar" title="cerrar"> </button>${mensaje}`
+
+    // agregamos el nodo(elemento/etiqueta) de boton al div
+    const elementoAlerta = document.getElementById('alerta')
+    elementoAlerta.append(etiqueta_envoltorio)
+
+    const botonCerrar = document.getElementById('boton-cerrar');
+
+    // agregamos un evento click al boton , para remover este elemento(notification) del arbol de elementos(DOM)
+    botonCerrar.addEventListener('click', function () {
+        etiqueta_envoltorio.remove();
+    });
 }
 
 function irAFuncionDeRetiro() {
@@ -126,9 +137,9 @@ function irAFuncionDeRetiro() {
         document.querySelector(".contenedor-retiro").style.display = "block";
         document.querySelector(".contenedor-deposito").style.display = "none";
         document.querySelector(".contenedor-consulta").style.display = "none";
-       // document.querySelector(".operacion-retiro").classList.add("text-white");
+        document.getElementById("datos-usuario").style.display = "none";
         console.log("Entro al if")
-    }else{
+    } else {
         console.log("error")
     }
 }
