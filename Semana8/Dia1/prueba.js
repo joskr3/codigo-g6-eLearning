1aeff9107d8b567b84d58d993cc91ee8c9fd2b0a
RobertoD <mitchelldurandb25@gmail.com>
2022-02-17 01:45:50 -0500

    mejoras al reto semana07 bootstrap

---
+++ Semana8/Dia1/prueba.js
@@ -0,0 +1,46 @@
+// let amigos = ["juan","pedro","rolando"]
+
+// function saludar() {
+//     for (let i = 0; i < array.length; i++) {
+//         console.log("hola "+amigos[i])
+        
+//     }
+//     if (i=2) {
+//         console.log("gracias por su visita")
+//     }
+// }
+// saludar();
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+
+
+// function numerosArray(...numeros) {
+//         console.log("Array Normal: "+numeros)
+//          let arrayInvertido = numeros.slice().reverse;
+//          console.log("Array Invertido: "+arrayInvertido)
+//          console.log("\n")
+  
+
+// }
+
+// numerosArray(35,25,45)
+
+
+function suma(numero, numero2, numero3) {
+        return numero + numero2 + numero3
+         
+}
+
+console.log(suma(15,20,35));
+
+resultado = 10;
+console.log(resultado)
+
+//FUNCION FLECHA
+let multiplicar=(a,b)=>a*b;
+console.log(multiplicar(3,8));
+
