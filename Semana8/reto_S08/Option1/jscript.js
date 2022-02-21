a6ee92b7b62309c9bde05164dba134755196acbd
RobertoD <mitchelldurandb25@gmail.com>
2022-02-20 20:26:34 -0500

    avance reto8

--- Semana8/reto_S08/Option1/jscript.js
+++ Semana8/reto_S08/Option1/jscript.js
@@ -2,6 +2,7 @@ let input = document.querySelector('#input');
 let btn = document.querySelector('#btn');
 let list = document.querySelector('#lista');
 let el = document.querySelector('li');
+let check = document.querySelector('#check');
 
 
 btn.addEventListener('click',() => {
@@ -10,9 +11,15 @@ btn.addEventListener('click',() => {
         alert('debes introducir alguna tarea');
     } else {
         let li = document.createElement('li');
-        //li.innerHTML = '<input type="checkbox" name="check" class="check" />';
-        li.innerHTML = '<input type="checkbox" name="check" class="check" />'+'<label for="check"> '+txt+'</label>';
-        list.insertBefore(li, list.childNodes[0]);
+        li.innerHTML = `
+        <input type="checkbox" name="check" class="check" />
+        <label for="check"> 
+        ${txt}
+        </label>
+        <div class="div-lista">  MINOR</div>
+        `;
+        list.appendChild(li);
+        //list.insertBefore(li, list.childNodes[0]);
         input.value = '';
     }
 })
