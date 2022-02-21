4c14c74457f89cfe28050feda42af077dabddcad
Javier Andres Moreno Alva <jamorenoalva@gmail.com>
2022-02-20 23:36:47 -0500

    Correccion

--- Semana8/Reto01/App.js
+++ Semana8/Reto01/App.js
@@ -8,7 +8,21 @@ class Tareas {
 }
 
 class UI {
-  anadirTarea() {}
+  anadirTarea(Tarea) {
+    const tareaList = document.getElementById("tarea-list");
+    const element = document.createElement('div');
+    element.innerHTML = `
+      <div class="card text-center mb-4">
+        <div class="card-body">
+          <strong>Tarea</strong>:${Tarea.tarea}
+          <strong>Prioridad</strong>:${Tarea.prioridad}
+          <strong>Urgencia</strong>:${Tarea.urgencia}
+          <strong>Duracion</strong>:${Tarea.duracion}
+        </div>
+      </div>
+    `;
+    tareaList.appendChild(element);
+  }
 
   eliminarTareas() {}
 
@@ -21,8 +35,10 @@ document.getElementById('tarea-form').addEventListener('submit', function (e) {
   const urgencia=document.getElementById('urgencia').value
   const duracion=document.getElementById('duracion').value
   // console.log(tarea + " - " + prioridad + " - " + urgencia + " - " + duracion);
-  const Tarea = new Tareas(tarea, prioridad, urgencia, duracion));
+  const Tarea = new Tareas(tarea, prioridad, urgencia, duracion);
 
+  const ui =new UI();
+  ui.anadirTarea(Tarea);
 
   e.preventDefault();
 });
